2a7e4681c5bf68d2e9d41ec4336e088b3c6a2451
vogje01 <jensvogt47@gmail.com>
2023-06-08 09:40:33 +0200

    Fix resource not found handling

--- awsmock/include/awsmock/controller/ResourceNotFound.h
+++ awsmock/include/awsmock/controller/ResourceNotFound.h
@@ -11,7 +11,7 @@
 #include "Poco/Net/HTTPRequestHandler.h"
 
 // AwsMock includes
-//#include "awsmock/resource/handling/JsonAPIErrorBuilder.h"
+#include "awsmock/dto/common/ResourceNotFounResponse.h"
 
 namespace AwsMock {
 
