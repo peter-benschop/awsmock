b2634889700835e45c90f4889d53953b708cfdbb
jensvogt <jensvogt47@gmail.com>
2023-09-20 17:30:34 +0200

    fix(180-s3-list-return-http-error-500): fix logging

---
+++ _deps/aws-mock-build/core/include/awsmock/core/Version.h
@@ -0,0 +1,14 @@
+//
+// Created by vogje01 on 16/06/2023.
+//
+
+#ifndef AWSMOCK_CORE_VERSION_H_IN_H
+#define AWSMOCK_CORE_VERSION_H_IN_H
+
+#define PROJECT_NAME "aws-mock"
+#define PROJECT_VER  "0.2.12"
+#define PROJECT_VER_MAJOR "0"
+#define PROJECT_VER_MINOR "2"
+#define PTOJECT_VER_PATCH "12"
+
+#endif //AWSMOCK_CORE_VERSION_H_IN_H
