56e03bc15cca09c6f0cffbaeef3281e77d5121fa
jensvogt <jensvogt47@gmail.com>
2023-09-22 18:32:54 +0200

    fix(180-s3-list-return-http-error-500): fix logging

---
+++ awsmock/include/awsmock/resource/HandlerException.h
@@ -0,0 +1,31 @@
+//
+// Created by vogje01 on 04/01/2023.
+//
+
+#ifndef AWSMOCK_RESOURCE_EXCEPTION_H
+#define AWSMOCK_RESOURCE_EXCEPTION_H
+
+// C++ standard includes
+#include <stdexcept>
+#include <utility>
+
+namespace AwsMock {
+
+    class HandlerException : public std::exception {
+    public:
+      HandlerException(std::string type, std::string message, int statusCode);
+
+      [[nodiscard]] int code() const;
+
+      [[nodiscard]] std::string type() const;
+
+      [[nodiscard]] std::string message() const;
+
+    private:
+      int _statusCode;
+      std::string _type;
+      std::string _message;
+    };
+} // namespace AwsMock
+
+#endif // AWSMOCK_RESOURCE_EXCEPTION_H
