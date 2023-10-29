4a682e4a7a54cd303640645373cb861c13a3416d
Jens Vogt <jens.vogt@opitz-consulting.com>
2023-10-29 12:34:53 +0100

    fix(185-lambdaservicehelper-shows-unencoded-characters-in-log-file): fix various bugs

--- src/service/include/awsmock/ftpserver/FtpSession.h
+++ src/service/include/awsmock/ftpserver/FtpSession.h
@@ -29,8 +29,7 @@
 #include <awsmock/ftpserver/FtpUser.h>
 #include <awsmock/service/AbstractWorker.h>
 
-#define DEFAULT_BASE_DIR "transfer"
-#define DEFAULT_TRANSFER_BUCKET "transfer-manager"
+#define DEFAULT_BASE_DIR "/home/awsmock/data/transfer"
 
 namespace AwsMock::FtpServer {
 
