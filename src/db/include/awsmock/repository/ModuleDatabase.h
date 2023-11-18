c2885da4772fd191c082031c6926105240d744e2
Jens Vogt <jens.vogt@opitz-consulting.com>
2023-11-18 17:10:18 +0100

    fix(185-lambdaservicehelper-shows-unencoded-characters-in-log-file): awslocal changes

--- src/db/include/awsmock/repository/ModuleDatabase.h
+++ src/db/include/awsmock/repository/ModuleDatabase.h
@@ -18,126 +18,131 @@ namespace AwsMock::Database {
 
   class ModuleDatabase : public Database {
 
-    public:
+  public:
 
-      /**
-       * Constructor
-       *
-       * @param configuration configuration properties
-       */
-      explicit ModuleDatabase(const Core::Configuration &configuration);
+    /**
+     * Constructor
+     *
+     * @param configuration configuration properties
+     */
+    explicit ModuleDatabase(const Core::Configuration &configuration);
 
-      /**
-       * Checks the active flag.
-       *
-       * @param name module name
-       * @return true if active
-       */
-      bool IsActive(const std::string &name);
+    /**
+     * Checks the active flag.
+     *
+     * @param name module name
+     * @return true if active
+     */
+    bool IsActive(const std::string &name);
 
-      /**
-       * Check existence of module
-       *
-       * @param name module name
-       * @return created module
-       */
-      bool ModuleExists(const std::string &name);
+    /**
+     * Check existence of module
+     *
+     * @param module module module
+     * @return created module
+     */
+    bool ModuleExists(const std::string &module);
 
-      /**
-       * Returns the module by id
-       *
-       * @param oid module oid
-       * @return module, if existing
-       * @throws DatabaseException
-       */
-      Entity::Module::Module GetModuleById(const bsoncxx::oid &oid);
+    /**
+     * Returns the module by id
+     *
+     * @param oid module oid
+     * @return module, if existing
+     * @throws DatabaseException
+     */
+    Entity::Module::Module GetModuleById(const bsoncxx::oid &oid);
 
-      /**
-       * Returns the module by id
-       *
-       * @param name module name
-       * @return module, if existing
-       * @throws DatabaseException
-       */
-      Entity::Module::Module GetModuleByName(const std::string &name);
+    /**
+     * Returns the module by id
+     *
+     * @param name module name
+     * @return module, if existing
+     * @throws DatabaseException
+     */
+    Entity::Module::Module GetModuleByName(const std::string &name);
 
-      /**
-       * Creates a module
-       *
-       * @param module module entity
-       * @return created module
-       */
-      Entity::Module::Module CreateModule(const Entity::Module::Module &module);
+    /**
+     * Creates a module
+     *
+     * @param module module entity
+     * @return created module
+     */
+    Entity::Module::Module CreateModule(const Entity::Module::Module &module);
 
-      /**
-       * Updates a module
-       *
-       * @param module module entity
-       * @return updated module
-       */
-      Entity::Module::Module UpdateModule(const Entity::Module::Module &module);
+    /**
+     * Updates a module
+     *
+     * @param module module entity
+     * @return updated module
+     */
+    Entity::Module::Module UpdateModule(const Entity::Module::Module &module);
 
-      /**
-       * Sets the status of module.
-       *
-       * @param name module name
-       * @param status module status
-       */
-      void SetStatus(const std::string &name, const Entity::Module::ModuleStatus &status);
+    /**
+     * Sets the status of module.
+     *
+     * @param name module name
+     * @param status module status
+     */
+    void SetStatus(const std::string &name, const Entity::Module::ModuleStatus &status);
 
-      /**
-       * Sets the port of module.
-       *
-       * @param name module name
-       * @param port module port
-       */
-      void SetPort(const std::string &name, int port);
+    /**
+     * Sets the port of module.
+     *
+     * @param name module name
+     * @param port module port
+     */
+    void SetPort(const std::string &name, int port);
 
-      /**
-       * Creates or updates a module
-       *
-       * @param module module entity
-       * @return updated module
-       */
-      Entity::Module::Module CreateOrUpdateModule(const Entity::Module::Module &module);
+    /**
+     * Creates or updates a modules
+     *
+     * @param modules modules entity
+     * @return updated modules
+     */
+    Entity::Module::Module CreateOrUpdateModule(const Entity::Module::Module &modules);
 
-      /**
-       * Counts the number of modules
-       *
-       * @return total number of modules
-       */
-      int ModuleCount();
+    /**
+     * Counts the number of modules
+     *
+     * @return total number of modules
+     */
+    int ModuleCount();
 
-      /**
-       * @brief Returns a list of all modules
-       *
-       * @return list of all modules
-       */
-      Entity::Module::ModuleList ListModules();
+    /**
+     * @brief Returns a list of all modules
+     *
+     * @return list of all modules
+     */
+    Entity::Module::ModuleList ListModules();
 
-      /**
-       * Deletes service
-       *
-       * @param module module entity
-       */
-      void DeleteModule(const Entity::Module::Module &module);
+    /**
+     * Deletes service
+     *
+     * @param module module entity
+     */
+    void DeleteModule(const Entity::Module::Module &module);
 
-      /**
-       * Deletes all modules
-       */
-      void DeleteAllModules();
+    /**
+     * Deletes all modules
+     */
+    void DeleteAllModules();
 
-    private:
+  private:
 
-      /**
-       * Logger
-       */
-      Core::LogStream _logger;
+    /**
+     * Logger
+     */
+    Core::LogStream _logger;
 
-      /**
-       * Service collection
-       */
-      mongocxx::collection _serviceCollection{};
+    /**
+     * Service collection
+     */
+    mongocxx::collection _moduleCollection{};
+
+    /**
+     * Existing modules
+     */
+    static std::map<std::string, Entity::Module::Module> _existingModules;
 
   };
 
--- src/db/include/awsmock/repository/S3Database.h
+++ src/db/include/awsmock/repository/S3Database.h
@@ -29,295 +29,295 @@ namespace AwsMock::Database {
 
   class S3Database : public Database {
 
-    public:
+  public:
 
-      /**
-       * Constructor
-       *
-       * @param configuration configuration properties
-       */
-      explicit S3Database(const Core::Configuration &configuration);
+    /**
+     * Constructor
+     *
+     * @param configuration configuration properties
+     */
+    explicit S3Database(const Core::Configuration &configuration);
 
-      /**
-       * Bucket exists
-       *
-       * @param region AWS region
-       * @param name bucket name
-       * @return true if bucket exists
-       * @throws DatabaseException
-       */
-      bool BucketExists(const std::string &region, const std::string &name);
+    /**
+     * Bucket exists
+     *
+     * @param region AWS region
+     * @param name bucket name
+     * @return true if bucket exists
+     * @throws DatabaseException
+     */
+    bool BucketExists(const std::string &region, const std::string &name);
 
-      /**
-       * Bucket exists
-       *
-       * @param bucket bucket entity
-       * @return true if bucket exists
-       * @throws DatabaseException
-       */
-      bool BucketExists(const Entity::S3::Bucket &bucket);
+    /**
+     * Bucket exists
+     *
+     * @param bucket bucket entity
+     * @return true if bucket exists
+     * @throws DatabaseException
+     */
+    bool BucketExists(const Entity::S3::Bucket &bucket);
 
-      /**
-       * Returns the total number of buckets
-       *
-       * @return total number of buckets
-       * @throws DatabaseException
-       */
-      long BucketCount();
+    /**
+     * Returns the total number of buckets
+     *
+     * @return total number of buckets
+     * @throws DatabaseException
+     */
+    long BucketCount();
 
-      /**
-       * Returns the bucket by id
-       *
-       * @param oid bucket oid
-       * @return bucket, if existing
-       * @throws DatabaseException
-       */
-      Entity::S3::Bucket GetBucketById(bsoncxx::oid oid);
+    /**
+     * Returns the bucket by id
+     *
+     * @param oid bucket oid
+     * @return bucket, if existing
+     * @throws DatabaseException
+     */
+    Entity::S3::Bucket GetBucketById(bsoncxx::oid oid);
 
-      /**
-       * Returns the bucket by id
-       *
-       * @param oid bucket oid
-       * @return bucket, if existing
-       * @throws DatabaseException
-       */
-      Entity::S3::Bucket GetBucketById(const std::string &oid);
+    /**
+     * Returns the bucket by id
+     *
+     * @param oid bucket oid
+     * @return bucket, if existing
+     * @throws DatabaseException
+     */
+    Entity::S3::Bucket GetBucketById(const std::string &oid);
 
-      /**
-       * Returns the bucket by region and name.
-       *
-       * @param region AWS region
-       * @param name bucket name
-       * @return bucket entity
-       */
-      Entity::S3::Bucket GetBucketByRegionName(const std::string &region, const std::string &name);
+    /**
+     * Returns the bucket by region and name.
+     *
+     * @param region AWS region
+     * @param name bucket name
+     * @return bucket entity
+     */
+    Entity::S3::Bucket GetBucketByRegionName(const std::string &region, const std::string &name);
 
-      /**
-       * Create a new bucket in the S3 bucket table
-       *
-       * @param bucket bucket entity
-       * @return created bucket entity
-       * @throws DatabaseException
-       */
-      Entity::S3::Bucket CreateBucket(const Entity::S3::Bucket &bucket);
+    /**
+     * Create a new bucket in the S3 bucket table
+     *
+     * @param bucket bucket entity
+     * @return created bucket entity
+     * @throws DatabaseException
+     */
+    Entity::S3::Bucket CreateBucket(const Entity::S3::Bucket &bucket);
 
-      /**
-       * List all buckets
-       *
-       * @return BucketList
-       */
-      Entity::S3::BucketList ListBuckets();
+    /**
+     * List all buckets
+     *
+     * @return BucketList
+     */
+    Entity::S3::BucketList ListBuckets();
 
-      /**
-       * CHeck whether the bucket has still objects
-       *
-       * @param bucket bucket entity
-       * @return true if bucket exists
-       * @throws DatabaseException
-       */
-      bool HasObjects(const Entity::S3::Bucket &bucket);
+    /**
+     * CHeck whether the bucket has still objects
+     *
+     * @param bucket bucket entity
+     * @return true if bucket exists
+     * @throws DatabaseException
+     */
+    bool HasObjects(const Entity::S3::Bucket &bucket);
 
-      /**
-       * Updates a bucket
-       *
-       * @param bucket bucket entity
-       * @return created bucket entity
-       * @throws DatabaseException
-       */
-      Entity::S3::Bucket UpdateBucket(const Entity::S3::Bucket &bucket);
+    /**
+     * Updates a bucket
+     *
+     * @param bucket bucket entity
+     * @return created bucket entity
+     * @throws DatabaseException
+     */
+    Entity::S3::Bucket UpdateBucket(const Entity::S3::Bucket &bucket);
 
-      /**
-       * Create a new S3 object in the S3 object table
-       *
-       * @param object object entity
-       * @return created object entity
-       * @throws DatabaseException
-       */
-      Entity::S3::Object CreateObject(const Entity::S3::Object &object);
+    /**
+     * Create a new S3 object in the S3 object table
+     *
+     * @param object object entity
+     * @return created object entity
+     * @throws DatabaseException
+     */
+    Entity::S3::Object CreateObject(const Entity::S3::Object &object);
 
-      /**
-       * Create a new S3 object in the S3 object table, if it does not exist, otherwise update the exiting object.
-       *
-       * @param object object entity
-       * @return created or updated object entity
-       * @throws DatabaseException
-       */
-      Entity::S3::Object CreateOrUpdateObject(const Entity::S3::Object &object);
+    /**
+     * Create a new S3 object in the S3 object table, if it does not exist, otherwise update the exiting object.
+     *
+     * @param object object entity
+     * @return created or updated object entity
+     * @throws DatabaseException
+     */
+    Entity::S3::Object CreateOrUpdateObject(const Entity::S3::Object &object);
 
-      /**
-       * Updates an existing object in the S3 object table
-       *
-       * @param object object entity
-       * @return updated object entity
-       * @throws DatabaseException
-       */
-      Entity::S3::Object UpdateObject(const Entity::S3::Object &object);
+    /**
+     * Updates an existing object in the S3 object table
+     *
+     * @param object object entity
+     * @return updated object entity
+     * @throws DatabaseException
+     */
+    Entity::S3::Object UpdateObject(const Entity::S3::Object &object);
 
-      /**
-       * Check the existence of an object
-       *
-       * @param object object entity
-       * @return true if existing otherwise false
-       * @throws DatabaseException
-       */
-      bool ObjectExists(const Entity::S3::Object &object);
+    /**
+     * Check the existence of an object
+     *
+     * @param object object entity
+     * @return true if existing otherwise false
+     * @throws DatabaseException
+     */
+    bool ObjectExists(const Entity::S3::Object &object);
 
-      /**
-       * Gets an object from an bucket
-       *
-       * @param region AWS S3 region name
-       * @param bucket object bucket
-       * @param key object key
-       * @return S3 object
-       * @throws DatabaseException
-       */
-      Entity::S3::Object GetObject(const std::string &region, const std::string &bucket, const std::string &key);
+    /**
+     * Gets an object from an bucket
+     *
+     * @param region AWS S3 region name
+     * @param bucket object bucket
+     * @param key object key
+     * @return S3 object
+     * @throws DatabaseException
+     */
+    Entity::S3::Object GetObject(const std::string &region, const std::string &bucket, const std::string &key);
 
-      /**
-       * Gets an object from a bucket using the bucket, key and MD5 sum as query parameter
-       *
-       * @param region AWS S3 region name
-       * @param bucket object bucket
-       * @param key object key
-       * @param md5sum MD5 sum of object
-       * @return S3 object
-       * @throws DatabaseException
-       */
-      Entity::S3::Object GetObjectMd5(const std::string &region, const std::string &bucket, const std::string &key, const std::string &md5sum);
+    /**
+     * Gets an object from a bucket using the bucket, key and MD5 sum as query parameter
+     *
+     * @param region AWS S3 region name
+     * @param bucket object bucket
+     * @param key object key
+     * @param md5sum MD5 sum of object
+     * @return S3 object
+     * @throws DatabaseException
+     */
+    Entity::S3::Object GetObjectMd5(const std::string &region, const std::string &bucket, const std::string &key, const std::string &md5sum);
 
-      /**
-       * Gets an object from a bucket using the bucket, key and MD5 sum as query parameter
-       *
-       * @param region AWS S3 region name
-       * @param bucket object bucket
-       * @param key object key
-       * @param version version ID
-       * @return S3 object
-       * @throws DatabaseException
-       */
-      Entity::S3::Object GetObjectVersion(const std::string &region, const std::string &bucket, const std::string &key, const std::string &version);
+    /**
+     * Gets an object from a bucket using the bucket, key and MD5 sum as query parameter
+     *
+     * @param region AWS S3 region name
+     * @param bucket object bucket
+     * @param key object key
+     * @param version version ID
+     * @return S3 object
+     * @throws DatabaseException
+     */
+    Entity::S3::Object GetObjectVersion(const std::string &region, const std::string &bucket, const std::string &key, const std::string &version);
 
-      /**
-       * Gets an object from an bucket
-       *
-       * @param oid object ID
-       * @return S3 object
-       * @throws DatabaseException
-       */
-      Entity::S3::Object GetObjectById(bsoncxx::oid oid);
+    /**
+     * Gets an object from an bucket
+     *
+     * @param oid object ID
+     * @return S3 object
+     * @throws DatabaseException
+     */
+    Entity::S3::Object GetObjectById(bsoncxx::oid oid);
 
-      /**
-       * Gets an object from an bucket
-       *
-       * @param oid object ID
-       * @return S3 object
-       * @throws DatabaseException
-       */
-      Entity::S3::Object GetObjectById(const std::string &oid);
+    /**
+     * Gets an object from an bucket
+     *
+     * @param oid object ID
+     * @return S3 object
+     * @throws DatabaseException
+     */
+    Entity::S3::Object GetObjectById(const std::string &oid);
 
-      /**
-       * List all objects of a bucket
-       *
-       * @param bucket S3 bucket name
-       * @param prefix S3 key prefix
-       * @return ObjectList
-       */
-      Entity::S3::ObjectList ListBucket(const std::string &bucket, const std::string &prefix = {});
+    /**
+     * List all objects of a bucket
+     *
+     * @param bucket S3 bucket name
+     * @param prefix S3 key prefix
+     * @return ObjectList
+     */
+    Entity::S3::ObjectList ListBucket(const std::string &bucket, const std::string &prefix = {});
 
-      /**
-       * List all objects.
-       *
-       * @param prefix S3 key prefix
-       * @return ObjectList
-       */
-      Entity::S3::ObjectList ListObjects(const std::string &prefix = {});
+    /**
+     * List all objects.
+     *
+     * @param prefix S3 key prefix
+     * @return ObjectList
+     */
+    Entity::S3::ObjectList ListObjects(const std::string &prefix = {});
 
-      /**
-       * Counts the number of keys in a bucket
-       *
-       * @param region AWS region
-       * @param bucket bucket name
-       * @return number of object in bucket
-       * @throws DatabaseException
-       */
-      long ObjectCount(const std::string &region = {}, const std::string &bucket = {});
+    /**
+     * Counts the number of keys in a bucket
+     *
+     * @param region AWS region
+     * @param bucket bucket name
+     * @return number of object in bucket
+     * @throws DatabaseException
+     */
+    long ObjectCount(const std::string &region = {}, const std::string &bucket = {});
 
-      /**
-       * Creates a bucket notification-
-       *
-       * <p>In case of a wildcard notification all notifications are added.</p>
-       *
-       * @param bucket S3 bucket
-       * @param bucketNotification bucket notification
-       * @return updated Bucket entity
-       */
-      Entity::S3::Bucket CreateBucketNotification(const Entity::S3::Bucket &bucket, const Entity::S3::BucketNotification &bucketNotification);
+    /**
+     * Creates a bucket notification-
+     *
+     * <p>In case of a wildcard notification all notifications are added.</p>
+     *
+     * @param bucket S3 bucket
+     * @param bucketNotification bucket notification
+     * @return updated Bucket entity
+     */
+    Entity::S3::Bucket CreateBucketNotification(const Entity::S3::Bucket &bucket, const Entity::S3::BucketNotification &bucketNotification);
 
-      /**
-       * Creates a bucket notification-
-       *
-       * @param bucket S3 bucket
-       * @param bucketNotification bucket notification
-       * @return updated Bucket entity
-       */
-      Entity::S3::Bucket DeleteBucketNotifications(const Entity::S3::Bucket &bucket, const Entity::S3::BucketNotification &bucketNotification);
+    /**
+     * Creates a bucket notification-
+     *
+     * @param bucket S3 bucket
+     * @param bucketNotification bucket notification
+     * @return updated Bucket entity
+     */
+    Entity::S3::Bucket DeleteBucketNotifications(const Entity::S3::Bucket &bucket, const Entity::S3::BucketNotification &bucketNotification);
 
-      /**
-       * Delete a bucket.
-       *
-       * @param bucket bucket entity
-       * @throws DatabaseException
-       */
-      void DeleteBucket(const Entity::S3::Bucket &bucket);
+    /**
+     * Delete a bucket.
+     *
+     * @param bucket bucket entity
+     * @throws DatabaseException
+     */
+    void DeleteBucket(const Entity::S3::Bucket &bucket);
 
-      /**
-       * Deletes all buckets
-       */
-      void DeleteAllBuckets();
+    /**
+     * Deletes all buckets
+     */
+    void DeleteAllBuckets();
 
-      /**
-       * Delete an object.
-       *
-       * @param object object entity
-       * @throws DatabaseException
-       */
-      void DeleteObject(const Entity::S3::Object &object);
+    /**
+     * Delete an object.
+     *
+     * @param object object entity
+     * @throws DatabaseException
+     */
+    void DeleteObject(const Entity::S3::Object &object);
 
-      /**
-       * Updates an existing object in the S3 object table
-       *
-       * @param bucket bucket to delete from
-       * @param keys vector of object keys
-       * @throws DatabaseException
-       */
-      void DeleteObjects(const std::string &bucket, const std::vector<std::string> &keys);
+    /**
+     * Updates an existing object in the S3 object table
+     *
+     * @param bucket bucket to delete from
+     * @param keys vector of object keys
+     * @throws DatabaseException
+     */
+    void DeleteObjects(const std::string &bucket, const std::vector<std::string> &keys);
 
-      /**
-       * Deletes all objects
-       */
-      void DeleteAllObjects();
+    /**
+     * Deletes all objects
+     */
+    void DeleteAllObjects();
 
-    private:
+  private:
 
-      /**
-       * Logger
-       */
-      Core::LogStream _logger;
+    /**
+     * Logger
+     */
+    Core::LogStream _logger;
 
-      /**
-       * S3 bucket collection
-       */
-      mongocxx::collection _bucketCollection{};
+    /**
+     * S3 bucket collection
+     */
+    mongocxx::collection _bucketCollection{};
 
-      /**
-       * S3 object collection
-       */
-      mongocxx::collection _objectCollection{};
+    /**
+     * S3 object collection
+     */
+    mongocxx::collection _objectCollection{};
 
-      /**
-       * Allowed event types
-       */
-      static std::map<std::string, std::vector<std::string>> allowedEventTypes;
+    /**
+     * Allowed event types
+     */
+    static std::map<std::string, std::vector<std::string>> allowedEventTypes;
 
   };
 
