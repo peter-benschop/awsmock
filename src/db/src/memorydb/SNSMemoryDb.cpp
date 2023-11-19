aa48e3b1d5a76c871b3d239834fc19b4f42adef5
Jens Vogt <jens.vogt@opitz-consulting.com>
2023-11-19 23:58:25 +0100

    fix(185-lambdaservicehelper-shows-unencoded-characters-in-log-file): add in-memory database for lambdas, sns, s3, transfer servers

--- src/db/src/memorydb/SNSMemoryDb.cpp
+++ src/db/src/memorydb/SNSMemoryDb.cpp
@@ -8,6 +8,44 @@ namespace AwsMock::Database {
 
   SNSMemoryDb::SNSMemoryDb() : _logger(Poco::Logger::get("SQSMemoryDb")) {}
 
+  bool SNSMemoryDb::TopicExists(const std::string &region, const std::string &name) {
+
+    return find_if(_topics.begin(), _topics.end(), [region, name](const std::pair<std::string, Entity::SNS::Topic> &topic) {
+      return topic.second.region == region && topic.second.topicName == name;
+    }) != _topics.end();
+
+  }
+
+  bool SNSMemoryDb::TopicExists(const std::string &name) {
+
+    return find_if(_topics.begin(), _topics.end(), [name](const std::pair<std::string, Entity::SNS::Topic> &topic) {
+      return topic.second.topicName == name;
+    }) != _topics.end();
+
+  }
+
+  Entity::SNS::Topic SNSMemoryDb::GetTopicById(const std::string &oid) {
+
+    auto it = find_if(_topics.begin(), _topics.end(), [oid](const std::pair<std::string, Entity::SNS::Topic> &queue) {
+      return queue.first == oid;
+    });
+
+    if (it != _topics.end()) {
+      it->second.oid = oid;
+      return it->second;
+    }
+    return {};
+  }
+
+  Entity::SNS::Topic SNSMemoryDb::CreateTopic(const Entity::SNS::Topic &topic) {
+
+    std::string oid = Poco::UUIDGenerator().createRandom().toString();
+    _topics[oid] = topic;
+    log_trace_stream(_logger) << "Topic created, oid: " << oid << std::endl;
+    return GetTopicById(oid);
+
+  }
+
   long SNSMemoryDb::CountTopics(const std::string &region) {
 
     long count = 0;
