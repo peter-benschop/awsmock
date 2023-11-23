f7fae9563188980b3c7d05d3a91279e21fd41602
Jens Vogt <jens.vogt@opitz-consulting.com>
2023-11-23 20:16:09 +0100

    fix(185-lambdaservicehelper-shows-unencoded-characters-in-log-file): fix unit tests

--- src/db/src/memorydb/TransferMemoryDb.cpp
+++ src/db/src/memorydb/TransferMemoryDb.cpp
@@ -13,6 +13,21 @@ namespace AwsMock::Database {
     return find_if(_transfers.begin(), _transfers.end(), [region, serverId](const std::pair<std::string, Entity::Transfer::Transfer> &transfer) {
       return transfer.second.region == region && transfer.second.serverId == serverId;
     }) != _transfers.end();
+
+  }
+
+  bool TransferMemoryDb::TransferExists(const Entity::Transfer::Transfer &transfer) {
+
+    TransferExists(transfer.region, transfer.serverId);
+
+  }
+
+  bool TransferMemoryDb::TransferExists(const std::string &serverId) {
+
+    return find_if(_transfers.begin(), _transfers.end(), [serverId](const std::pair<std::string, Entity::Transfer::Transfer> &transfer) {
+      return transfer.second.serverId == serverId;
+    }) != _transfers.end();
+
   }
 
   std::vector<Entity::Transfer::Transfer> TransferMemoryDb::ListServers(const std::string &region) {
@@ -24,5 +39,84 @@ namespace AwsMock::Database {
 
     log_trace_stream(_logger) << "Got transfer list, size: " << transferList.size() << std::endl;
     return transferList;
+
+  }
+
+  Entity::Transfer::Transfer TransferMemoryDb::CreateTransfer(const Entity::Transfer::Transfer &transfer) {
+    Poco::ScopedLock lock(_transferMutex);
+
+    std::string oid = Poco::UUIDGenerator().createRandom().toString();
+    _transfers[oid] = transfer;
+    log_trace_stream(_logger) << "Transfer created, oid: " << oid << std::endl;
+    return GetTransferById(oid);
+  }
+
+  Entity::Transfer::Transfer TransferMemoryDb::UpdateTransfer(const Entity::Transfer::Transfer &transfer) {
+    Poco::ScopedLock lock(_transferMutex);
+
+    std::string region = transfer.region;
+    std::string serverId = transfer.serverId;
+    auto it = find_if(_transfers.begin(), _transfers.end(), [region, serverId](const std::pair<std::string, Entity::Transfer::Transfer> &transfer) {
+      return transfer.second.region == region && transfer.second.serverId == serverId;
+    });
+    _transfers[it->first] = transfer;
+    return _transfers[it->first];
+
+  }
+
+  Entity::Transfer::Transfer TransferMemoryDb::GetTransferById(const std::string &oid) {
+
+    auto it = find_if(_transfers.begin(), _transfers.end(), [oid](const std::pair<std::string, Entity::Transfer::Transfer> &transfer) {
+      return transfer.first == oid;
+    });
+
+    if (it != _transfers.end()) {
+      it->second.oid = oid;
+      return it->second;
+    }
+    return {};
+  }
+
+  Entity::Transfer::Transfer TransferMemoryDb::GetTransferByServerId(const std::string &serverId) {
+
+    auto it = find_if(_transfers.begin(), _transfers.end(), [serverId](const std::pair<std::string, Entity::Transfer::Transfer> &transfer) {
+      return transfer.second.serverId == serverId;
+    });
+
+    if (it != _transfers.end()) {
+      it->second.oid = it->first;
+      return it->second;
+    }
+    return {};
+  }
+
+  Entity::Transfer::Transfer TransferMemoryDb::GetTransferByArn(const std::string &arn) {
+
+    auto it = find_if(_transfers.begin(), _transfers.end(), [arn](const std::pair<std::string, Entity::Transfer::Transfer> &transfer) {
+      return transfer.second.serverId == arn;
+    });
+
+    if (it != _transfers.end()) {
+      it->second.oid = it->first;
+      return it->second;
+    }
+    return {};
+  }
+
+  void TransferMemoryDb::DeleteTransfer(const std::string &serverId) {
+    Poco::ScopedLock lock(_transferMutex);
+
+    const auto count = std::erase_if(_transfers, [serverId](const auto &item) {
+      auto const &[key, value] = item;
+      return value.serverId == serverId;
+    });
+    log_debug_stream(_logger) << "Transfer server deleted, count: " << count << std::endl;
+  }
+
+  void TransferMemoryDb::DeleteAllTransfers() {
+    Poco::ScopedLock lock(_transferMutex);
+
+    log_debug_stream(_logger) << "All transfer servers deleted, count: " << _transfers.size() << std::endl;
+    _transfers.clear();
   }
 }
