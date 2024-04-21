e76c5740c1a034851b4cf2bec8e5db3272320264
Jens Vogt <jens.vogt@opitz-consulting.com>
2024-04-22 00:10:22 +0200

    feat(391-support-for-secretsmanager): cleanup monitoring

--- src/dto/src/dynamodb/PutItemResponse.cpp
+++ src/dto/src/dynamodb/PutItemResponse.cpp
@@ -6,51 +6,50 @@
 
 namespace AwsMock::Dto::DynamoDb {
 
-  std::string PutItemResponse::ToJson() {
+    std::string PutItemResponse::ToJson() const {
 
-    try {
-      Poco::JSON::Object rootJson;
-      rootJson.set("Region", region);
-      rootJson.set("TableName", tableName);
+        try {
+            Poco::JSON::Object rootJson;
+            rootJson.set("Region", region);
+            rootJson.set("TableName", tableName);
 
-      std::ostringstream os;
-      rootJson.stringify(os);
-      return os.str();
+            return Core::JsonUtils::ToJsonString(rootJson);
 
-    } catch (Poco::Exception &exc) {
-      throw Core::ServiceException(exc.message(), Poco::Net::HTTPServerResponse::HTTP_INTERNAL_SERVER_ERROR);
+        } catch (Poco::Exception &exc) {
+            log_error << exc.message();
+            throw Core::JsonException(exc.message());
+        }
     }
-  }
 
-  void PutItemResponse::FromJson(const std::string &jsonString) {
+    void PutItemResponse::FromJson(const std::string &jsonString) {
 
-    body = jsonString;
+        body = jsonString;
 
-    Poco::JSON::Parser parser;
-    Poco::Dynamic::Var result = parser.parse(jsonString);
-    Poco::JSON::Object::Ptr rootObject = result.extract<Poco::JSON::Object::Ptr>();
+        Poco::JSON::Parser parser;
+        Poco::Dynamic::Var result = parser.parse(jsonString);
+        Poco::JSON::Object::Ptr rootObject = result.extract<Poco::JSON::Object::Ptr>();
 
-    try {
+        try {
 
-      //Poco::JSON::Object::Ptr jsonTableDescription = rootObject->getObject("TableDescription");
+            //Poco::JSON::Object::Ptr jsonTableDescription = rootObject->getObject("TableDescription");
 
-      //Core::JsonUtils::GetJsonValueString("TableName", jsonTableDescription, tableName);
+            //Core::JsonUtils::GetJsonValueString("TableName", jsonTableDescription, tableName);
 
-    } catch (Poco::Exception &exc) {
-      std::cerr << exc.message()  <<std::endl;
-      throw Core::ServiceException(exc.message(), Poco::Net::HTTPServerResponse::HTTP_INTERNAL_SERVER_ERROR);
+        } catch (Poco::Exception &exc) {
+            log_error << exc.message();
+            throw Core::JsonException(exc.message());
+        }
     }
-  }
 
-  std::string PutItemResponse::ToString() const {
-    std::stringstream ss;
-    ss << (*this);
-    return ss.str();
-  }
+    std::string PutItemResponse::ToString() const {
+        std::stringstream ss;
+        ss << (*this);
+        return ss.str();
+    }
 
-  std::ostream &operator<<(std::ostream &os, const PutItemResponse &r) {
-    os << "PutItemResponse={region='" << r.region << "', tableName='" << r.tableName << "}";
-    return os;
-  }
+    std::ostream &operator<<(std::ostream &os, const PutItemResponse &r) {
+        os << "PutItemResponse=" << r.ToJson();
+        return os;
+    }
 
 } // namespace AwsMock::Dto::DynamoDb
