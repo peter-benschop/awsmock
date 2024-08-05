0aa979621ded14db448258e0d6982427c0125546
Jens Vogt <jens.vogt@opitz-consulting.com>
2024-08-05 15:12:17 +0200

    docs(545-add-cognito-sign-up-feature): add new samples

--- dist/samples/cognito/delete-user-pool-client.sh
+++ dist/samples/cognito/delete-user-pool-client.sh
@@ -6,13 +6,13 @@ alias awslocal="aws --region eu-central-1 --endpoint --endpoint http://localhost
 userPoolId=$(awslocal cognito-idp create-user-pool --pool-name test-user-pool | jq -r '.UserPool.Id')
 
 # Create user pool client
-awslocal cognito-idp create-user-pool-client --user-pool-id $userPoolId --client-name test-client --generate-secret
+awslocal cognito-idp create-user-pool-client --user-pool-id "$userPoolId" --client-name test-client --generate-secret
 
 # List user pool clients
-clientId=$(awslocal cognito-idp list-user-pool-clients --user-pool-id $userPoolId | jq -r '.UserPoolClients[0].ClientId')
+clientId=$(awslocal cognito-idp list-user-pool-clients --user-pool-id "$userPoolId" | jq -r '.UserPoolClients[0].ClientId')
 
 # Describe user pool client
-awslocal cognito-idp describe-user-pool-client --user-pool-id $userPoolId --client-id $clientId
+awslocal cognito-idp describe-user-pool-client --user-pool-id "$userPoolId" --client-id "$clientId"
 
 # Delete the user-pool
-awslocal cognito-idp delete-user-pool --user-pool-id $userPoolId
+awslocal cognito-idp delete-user-pool --user-pool-id "$userPoolId"
--- dist/samples/cognito/list-users-in-group.sh
+++ dist/samples/cognito/list-users-in-group.sh
@@ -6,13 +6,13 @@ alias awslocal="aws --region eu-central-1 --endpoint --endpoint http://localhost
 userPoolId=$(awslocal cognito-idp create-user-pool --pool-name test-user-pool | jq -r '.UserPool.Id')
 
 # Create group
-awslocal cognito-idp create-group --user-pool-id $userPoolId --group-name test-group
+awslocal cognito-idp create-group --user-pool-id "$userPoolId" --group-name test-group
 
 # Create user in user pool
-awslocal cognito-idp admin-create-user --user-pool-id $userPoolId --username test-user
+awslocal cognito-idp admin-create-user --user-pool-id "$userPoolId" --username test-user
 
 # Add user to group
-awslocal cognito-idp admin-add-user-to-group --user-pool-id $userPoolId --username test-user --group-name test-group
+awslocal cognito-idp admin-add-user-to-group --user-pool-id "$userPoolId" --username test-user --group-name test-group
 
 # List users in group
-awslocal cognito-idp list-users-in-group --user-pool-id $userPoolId --group-name test-group
+awslocal cognito-idp list-users-in-group --user-pool-id "$userPoolId" --group-name test-group
