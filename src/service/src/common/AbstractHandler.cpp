2a644be0d26b85844ba325992451927884b8b211
Jens Vogt <jens.vogt@opitz-consulting.com>
2024-10-31 19:35:53 +0100

    fix(653-move-manager-apis-to-gateway): add SNS topic counters

--- src/service/src/common/AbstractHandler.cpp
+++ src/service/src/common/AbstractHandler.cpp
@@ -38,7 +38,7 @@ namespace AwsMock::Service {
         response.set(http::field::keep_alive, "false");
         response.set(http::field::content_type, "application/json");
         response.set(http::field::date, Core::DateTimeUtils::HttpFormat());
-        response.set(http::field::access_control_allow_origin, "http://localhost:4200");
+        response.set(http::field::access_control_allow_origin, "*");
         response.set(http::field::access_control_allow_headers, "cache-control,content-type,x-amz-target,x-amz-user-agent");
         response.set(http::field::access_control_allow_methods, "GET,PUT,POST,DELETE,HEAD,OPTIONS");
 
@@ -69,7 +69,7 @@ namespace AwsMock::Service {
         response.set(http::field::content_type, "application/json");
         response.set(http::field::content_length, std::to_string(contentLength));
         response.set(http::field::date, Core::DateTimeUtils::HttpFormat());
-        response.set(http::field::access_control_allow_origin, "http://localhost:4200");
+        response.set(http::field::access_control_allow_origin, "*");
         response.set(http::field::access_control_allow_headers, "cache-control,content-type,x-amz-target,x-amz-user-agent");
         response.set(http::field::access_control_allow_methods, "GET,PUT,POST,DELETE,HEAD,OPTIONS");
 
@@ -100,7 +100,7 @@ namespace AwsMock::Service {
         response.set(http::field::keep_alive, "false");
         response.set(http::field::content_type, "application/json");
         response.set(http::field::date, Core::DateTimeUtils::HttpFormat());
-        response.set(http::field::access_control_allow_origin, "http://localhost:4200");
+        response.set(http::field::access_control_allow_origin, "*");
         response.set(http::field::access_control_allow_headers, "cache-control,content-type,x-amz-target,x-amz-user-agent");
         response.set(http::field::access_control_allow_methods, "GET,PUT,POST,DELETE,HEAD,OPTIONS");
 
@@ -125,7 +125,7 @@ namespace AwsMock::Service {
         response.set(http::field::keep_alive, "false");
         response.set(http::field::content_type, "application/json");
         response.set(http::field::date, Core::DateTimeUtils::HttpFormat());
-        response.set(http::field::access_control_allow_origin, "http://localhost:4200");
+        response.set(http::field::access_control_allow_origin, "*");
         response.set(http::field::access_control_allow_headers, "cache-control,content-type,x-amz-target,x-amz-user-agent");
         response.set(http::field::access_control_allow_methods, "GET,PUT,POST,DELETE,HEAD,OPTIONS");
 
@@ -154,7 +154,7 @@ namespace AwsMock::Service {
         response.set(http::field::keep_alive, "false");
         response.set(http::field::content_type, "application/json");
         response.set(http::field::date, Core::DateTimeUtils::HttpFormat());
-        response.set(http::field::access_control_allow_origin, "http://localhost:4200");
+        response.set(http::field::access_control_allow_origin, "*");
         response.set(http::field::access_control_allow_headers, "cache-control,content-type,x-amz-target,x-amz-user-agent");
         response.set(http::field::access_control_allow_methods, "GET,PUT,POST,DELETE,HEAD,OPTIONS");
 
@@ -194,7 +194,7 @@ namespace AwsMock::Service {
             response.set(http::field::keep_alive, "false");
             response.set(http::field::content_type, "application/octet-stream");
             response.set(http::field::date, Core::DateTimeUtils::HttpFormat());
-            response.set(http::field::access_control_allow_origin, "http://localhost:4200");
+            response.set(http::field::access_control_allow_origin, "*");
             response.set(http::field::access_control_allow_headers, "cache-control,content-type,x-amz-target,x-amz-user-agent");
             response.set(http::field::access_control_allow_methods, "GET,PUT,POST,DELETE,HEAD,OPTIONS");
 
