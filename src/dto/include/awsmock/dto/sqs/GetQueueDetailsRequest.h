b8ab43a99c8650b13f8605ac9d6c2923aa6744df
Jens Vogt <jens.vogt@opitz-consulting.com>
2024-12-11 01:14:38 +0100

    fix(653-move-manager-apis-to-gateway): remove POCO XML

--- src/dto/include/awsmock/dto/sqs/GetQueueDetailsRequest.h
+++ src/dto/include/awsmock/dto/sqs/GetQueueDetailsRequest.h
@@ -9,9 +9,9 @@
 #include <string>
 
 // AwsMock includes
-#include <awsmock/core/exception/ServiceException.h>
 #include <awsmock/core/BsonUtils.h>
 #include <awsmock/core/LogStream.h>
+#include <awsmock/core/exception/ServiceException.h>
 
 namespace AwsMock::Dto::SQS {
 
