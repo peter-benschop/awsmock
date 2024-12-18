8a284680c69714b3de5d7906e7ab98fd26e249cb
Jens Vogt <jens.vogt@opitz-consulting.com>
2024-12-18 23:10:18 +0100

    fix(653-move-manager-apis-to-gateway): fix DeleteMessageBatchRequest.cpp

--- src/dto/src/secretsmanager/Secret.cpp
+++ src/dto/src/secretsmanager/Secret.cpp
@@ -8,46 +8,61 @@ namespace AwsMock::Dto::SecretsManager {
 
     view_or_value<view, value> Secret::ToDocument() const {
 
-        /* Todo:
         try {
 
-            Poco::JSON::Object rootJson;
-            rootJson.set("ARN", arn);
-            rootJson.set("Name", name);
-            rootJson.set("Description", description);
-            rootJson.set("KmsKeyId", kmsKeyId);
-            rootJson.set("OwningService", owningService);
-            rootJson.set("PrimaryRegion", primaryRegion);
+            document document;
+            Core::Bson::BsonUtils::SetStringValue(document, "Name", name);
+            Core::Bson::BsonUtils::SetStringValue(document, "ARN", arn);
+            Core::Bson::BsonUtils::SetStringValue(document, "Description", description);
+            Core::Bson::BsonUtils::SetStringValue(document, "KmsKeyId", kmsKeyId);
+            Core::Bson::BsonUtils::SetStringValue(document, "OwningService", owningService);
+            Core::Bson::BsonUtils::SetStringValue(document, "PrimaryRegion", primaryRegion);
+            Core::Bson::BsonUtils::SetLongValue(document, "CreatedDate", createdDate);
+            Core::Bson::BsonUtils::SetLongValue(document, "DeletedDate", deletedDate);
+            Core::Bson::BsonUtils::SetLongValue(document, "LastAccessedDate", lastAccessedDate);
+            Core::Bson::BsonUtils::SetLongValue(document, "LastChangedDate", lastChangedDate);
+            Core::Bson::BsonUtils::SetLongValue(document, "LastRotatedDate", lastRotatedDate);
+            Core::Bson::BsonUtils::SetLongValue(document, "NextRotatedDate", nextRotatedDate);
+            Core::Bson::BsonUtils::SetBoolValue(document, "RotationEnabled", rotationEnabled);
+            Core::Bson::BsonUtils::SetStringValue(document, "RotationLambdaARN", rotationLambdaARN);
 
-            rootJson.set("CreatedDate", createdDate);
-            rootJson.set("DeletedDate", deletedDate);
-            rootJson.set("LastAccessedDate", lastAccessedDate);
-            rootJson.set("LastChangedDate", lastChangedDate);
-            rootJson.set("LastRotatedDate", lastRotatedDate);
-            rootJson.set("NextRotatedDate", nextRotatedDate);
+            document.append(kvp("RotationRules", rotationRules.ToDocument()));
 
-            rootJson.set("RotationEnabled", rotationEnabled);
-            rootJson.set("RotationLambdaARN", rotationLambdaARN);
+            return document.extract();
 
-            rootJson.set("RotationRules", rotationRules.ToJsonObject());
-
-            return rootJson;
-
-        } catch (Poco::Exception &exc) {
-            throw Core::JsonException(exc.message());
-        }*/
-        return {};
+        } catch (bsoncxx::exception &exc) {
+            log_error << exc.what();
+            throw Core::JsonException(exc.what());
+        }
     }
 
     void Secret::FromDocument(const view_or_value<view, value> &document) {
-        /* Todo:
+
         try {
 
-            Core::JsonUtils::GetJsonValueString("ARN", jsonObject, arn);
+            name = Core::Bson::BsonUtils::GetStringValue(document, "Name");
+            arn = Core::Bson::BsonUtils::GetStringValue(document, "ARN");
+            description = Core::Bson::BsonUtils::GetStringValue(document, "Description");
+            kmsKeyId = Core::Bson::BsonUtils::GetStringValue(document, "KmsKeyId");
+            owningService = Core::Bson::BsonUtils::GetStringValue(document, "OwningService");
+            primaryRegion = Core::Bson::BsonUtils::GetStringValue(document, "PrimaryRegion");
+            createdDate = Core::Bson::BsonUtils::GetLongValue(document, "CreatedDate");
+            deletedDate = Core::Bson::BsonUtils::GetLongValue(document, "DeletedDate");
+            lastAccessedDate = Core::Bson::BsonUtils::GetLongValue(document, "LastAccessedDate");
+            lastChangedDate = Core::Bson::BsonUtils::GetLongValue(document, "LastChangedDate");
+            lastRotatedDate = Core::Bson::BsonUtils::GetLongValue(document, "LastRotatedDate");
+            nextRotatedDate = Core::Bson::BsonUtils::GetLongValue(document, "NextRotatedDate");
+            rotationEnabled = Core::Bson::BsonUtils::GetBoolValue(document, "RotationEnabled");
+            rotationLambdaARN = Core::Bson::BsonUtils::GetStringValue(document, "RotationLambdaARN");
 
-        } catch (Poco::Exception &exc) {
-            throw Core::JsonException(exc.message());
-        }*/
+            if (document.view().find("RotationRules") != document.view().end()) {
+                rotationRules.FromDocument(document.view()["RotationRules"].get_document().value);
+            }
+
+        } catch (bsoncxx::exception &exc) {
+            log_error << exc.what();
+            throw Core::JsonException(exc.what());
+        }
     }
 
     std::string Secret::ToJson() const {
