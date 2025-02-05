4adeb6345b346187119c3981e822e45155435ead
jensvogt <132199303+jensvogt@users.noreply.github.com>
2025-02-05 16:11:56 +0100

    Merge pull request #943
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * Merge branch '861-lambda-invocation' of https://github.com/jensvogt/a…
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed SQS notification event handling.
    
    * fix(861-lambda-invocation): fixed lambda delete issues.
    
    * fix(861-lambda-invocation): fixed binary uploads S3 putobject.
    
    * fix(861-lambda-invocation): fixed binary uploads S3 putobject.
    
    * Merge branch 'main' into 861-lambda-invocation
    
    * Merge branch '861-lambda-invocation' of https://github.com/jensvogt/a…
    
    * Merge branch 'main' into 861-lambda-invocation
    
    * fix(861-lambda-invocation): upgrade boost version to 1.87
    
    * fix(861-lambda-invocation): upgrade boost version to 1.87
    
    * fix(861-lambda-invocation): upgrade boost version to 1.87
    
    * fix(861-lambda-invocation): upgrade boost version to 1.87
    
    * fix(861-lambda-invocation): fix lambda instance cleanup
    
    * Merge branch 'main' into 861-lambda-invocation
    
    * Merge branch 'main' into 861-lambda-invocation
    
    * fix(861-lambda-invocation): fix lambda instance cleanup
    
    * fix(861-lambda-invocation): upgrade boost version to 1.87
    
    * fix(861-lambda-invocation): fix lambda instance cleanup
    
    * fix(861-lambda-invocation): fix lambda instance cleanup
    
    * fix(861-lambda-invocation): fix lambda instance cleanup
    
    * Merge branch 'main' into 861-lambda-invocation
    
    * Merge remote-tracking branch 'origin/861-lambda-invocation' into 861-…
    
    * fix(861-lambda-invocation): fix lambda instance cleanup

--- src/dto/src/sqs/mapper/Mapper.cpp
+++ src/dto/src/sqs/mapper/Mapper.cpp
@@ -6,7 +6,7 @@
 
 namespace AwsMock::Dto::SQS {
 
-    std::vector<std::string> Mapper::excludedAttributeNames = {"contentType", "id", "timestamp"};
+    //    std::vector<std::string> Mapper::excludedAttributeNames = {"contentType", "id", "timestamp"};
 
     Database::Entity::SQS::Message Mapper::map(const SendMessageRequest &request) {
 
@@ -60,13 +60,11 @@ namespace AwsMock::Dto::SQS {
     Database::Entity::SQS::MessageAttributeList Mapper::map(const MessageAttributeList &messageAttributes) {
         Database::Entity::SQS::MessageAttributeList messageAttributeList{};
         for (const auto &[fst, snd]: messageAttributes) {
-            if (std::ranges::find(excludedAttributeNames.begin(), excludedAttributeNames.end(), fst) == excludedAttributeNames.end()) {
                 Database::Entity::SQS::MessageAttribute messageAttribute;
                 messageAttribute.attributeName = fst;
                 messageAttribute.attributeValue = snd.stringValue;
                 messageAttribute.attributeType = Database::Entity::SQS::MessageAttributeTypeFromString(MessageAttributeDataTypeToString(snd.type));
                 messageAttributeList.emplace_back(messageAttribute);
-            }
         }
         return messageAttributeList;
     }
@@ -75,13 +73,11 @@ namespace AwsMock::Dto::SQS {
         MessageAttributeList messageAttributeList{};
         if (!messageAttributes.empty()) {
             for (const auto &[attributeName, attributeValue, attributeType]: messageAttributes) {
-                if (std::ranges::find(excludedAttributeNames.begin(), excludedAttributeNames.end(), attributeName) == excludedAttributeNames.end()) {
-                    MessageAttribute messageAttribute;
-                    messageAttribute.name = attributeName;
-                    messageAttribute.type = MessageAttributeDataTypeFromString(MessageAttributeTypeToString(attributeType));
-                    messageAttribute.stringValue = attributeValue;
-                    messageAttributeList[attributeName] = messageAttribute;
-                }
+                MessageAttribute messageAttribute;
+                messageAttribute.name = attributeName;
+                messageAttribute.type = MessageAttributeDataTypeFromString(MessageAttributeTypeToString(attributeType));
+                messageAttribute.stringValue = attributeValue;
+                messageAttributeList[attributeName] = messageAttribute;
             }
         }
         return messageAttributeList;
