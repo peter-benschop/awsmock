d520170eddc8f41b82d2c2c9ccbbaef7133fb28f
peter-benschop <163882280+peter-benschop@users.noreply.github.com>
2025-05-07 13:26:42 +0200

    Fix: json string

--- src/service/include/awsmock/service/sns/SNSService.h
+++ src/service/include/awsmock/service/sns/SNSService.h
@@ -15,6 +15,8 @@
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
 #include <boost/beast/version.hpp>
+#include <boost/json.hpp>
+#include <boost/json/src.hpp>
 
 // AwsMock includes
 #include <awsmock/core/AwsUtils.h>
